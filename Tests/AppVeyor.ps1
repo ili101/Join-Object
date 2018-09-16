e66858ad4b6e3a160a53815118d3d0deabc9c65a
ili101 <zoby101@gmail.com>
2018-09-16 19:27:36 +0300

    AppVeyor Version

--- Tests/AppVeyor.ps1
+++ Tests/AppVeyor.ps1
@@ -3,15 +3,62 @@ param
     [Switch]$Finalize
 )
 
+function Get-EnvironmentInfo
+{
+    $Lookup = @{
+        378389 = [version]'4.5'
+        378675 = [version]'4.5.1'
+        378758 = [version]'4.5.1'
+        379893 = [version]'4.5.2'
+        393295 = [version]'4.6'
+        393297 = [version]'4.6'
+        394254 = [version]'4.6.1'
+        394271 = [version]'4.6.1'
+        394802 = [version]'4.6.2'
+        394806 = [version]'4.6.2'
+        460798 = [version]'4.7'
+        460805 = [version]'4.7'
+        461308 = [version]'4.7.1'
+        461310 = [version]'4.7.1'
+        461808 = [version]'4.7.2'
+        461814 = [version]'4.7.2'
+    }
+
+    # For extra effect we could get the Windows 10 OS version and build release id:
+    try
+    {
+        $WinRelease, $WinVer = Get-ItemPropertyValue "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" ReleaseId, CurrentMajorVersionNumber, CurrentMinorVersionNumber, CurrentBuildNumber, UBR
+        $WindowsVersion = "$($WinVer -join '.') ($WinRelease)"
+    }
+    catch
+    {
+        $WindowsVersion = [System.Environment]::OSVersion.Version
+    }
+
+    Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
+        Get-ItemProperty -Name Version, Release -ErrorAction SilentlyContinue |
+        # For The One True framework (latest .NET 4x), change match to PSChildName -eq "Full":
+    Where-Object { $_.PSChildName -eq "Full"} |
+        Select-Object @{name = ".NET Framework"; expression = {$_.PSChildName}},
+    @{name = "Product"; expression = {$Lookup[$_.Release]}},
+    Version, Release,
+    # Some OPTIONAL extra output: PSComputerName and WindowsVersion
+    # The Computer name, so output from local machines will match remote machines:
+    @{ name = "PSComputerName"; expression = {$Env:Computername}},
+    # The Windows Version (works on Windows 10, at least):
+    @{ name = "WindowsVersion"; expression = { $WindowsVersion }},
+    @{ name = "PSVersion"; expression = { $PSVersionTable.PSVersion }}
+}
+
 # Initialize
-$PSVersion = $PSVersionTable.PSVersion.Major
-$TestFile = "TestResultsPS$PSVersion.xml"
+$TestFile = "TestResultsPS{0}.xml" -f $PSVersionTable.PSVersion
 #$VerbosePreference = 'Continue'
 
 # Run a test with the current version of PowerShell
-if(!$Finalize)
+if (!$Finalize)
 {
-    "[Progress] Testing with PowerShell $PSVersion"
+    "[Progress] Testing On:"
+    ((Get-EnvironmentInfo) | Out-String).Trim()
     . .\Install.ps1
     Invoke-Pester -OutputFile $TestFile
 }
