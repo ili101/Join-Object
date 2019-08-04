345292e88c4642e6cff961106fdce2bf236dd1f4
ili101 <zoby101@gmail.com>
2019-08-04 17:57:47 +0300

    Fix Assert Linux

--- Tests/Assert.ps1
+++ Tests/Assert.ps1
@@ -3,12 +3,12 @@
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Branch = 'Test' #'master'
 $Repository = 'Assert'
-$PathZip = Join-Path $env:TEMP "$Repository-$Branch.zip"
-$PathPsd1 = Join-Path $env:TEMP "$Repository-$Branch" | Join-Path -ChildPath "$Repository.psd1"
+$PathZip = Join-Path ([System.IO.Path]::GetTempPath()) "$Repository-$Branch.zip"
+$PathPsd1 = Join-Path ([System.IO.Path]::GetTempPath()) "$Repository-$Branch" | Join-Path -ChildPath "$Repository.psd1"
 
 if (!(Test-Path $PathPsd1)) {
     Invoke-WebRequest "https://github.com/ili101/$Repository/archive/$Branch.zip" -OutFile $PathZip
     #[Net.ServicePointManager]::SecurityProtocol = $SecurityProtocol
-    Expand-Archive -Path $PathZip -DestinationPath $env:TEMP
+    Expand-Archive -Path $PathZip -DestinationPath ([System.IO.Path]::GetTempPath())
 }
 Import-Module -Name $PathPsd1
