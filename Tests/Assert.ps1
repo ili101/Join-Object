bdf89d9826b8272fcf5c754e1adb93354470cf11
ili101 <zoby101@gmail.com>
2019-08-04 18:02:45 +0300

    [skip azp][skip av]

--- Tests/Assert.ps1
+++ Tests/Assert.ps1
@@ -3,12 +3,12 @@
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Branch = 'Test' #'master'
 $Repository = 'Assert'
-$PathZip = Join-Path ([System.IO.Path]::GetTempPath()) "$Repository-$Branch.zip"
-$PathPsd1 = Join-Path ([System.IO.Path]::GetTempPath()) "$Repository-$Branch" | Join-Path -ChildPath "$Repository.psd1"
+$PathZip = Join-Path ([IO.Path]::GetTempPath()) "$Repository-$Branch.zip"
+$PathPsd1 = Join-Path ([IO.Path]::GetTempPath()) "$Repository-$Branch" | Join-Path -ChildPath "$Repository.psd1"
 
 if (!(Test-Path $PathPsd1)) {
     Invoke-WebRequest "https://github.com/ili101/$Repository/archive/$Branch.zip" -OutFile $PathZip
     #[Net.ServicePointManager]::SecurityProtocol = $SecurityProtocol
-    Expand-Archive -Path $PathZip -DestinationPath ([System.IO.Path]::GetTempPath())
+    Expand-Archive -Path $PathZip -DestinationPath ([IO.Path]::GetTempPath())
 }
 Import-Module -Name $PathPsd1
