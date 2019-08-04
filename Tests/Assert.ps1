60146dc2d44819cd60e115ecbbad7dae0f9f1b57
ili101 <zoby101@gmail.com>
2019-08-04 14:59:35 +0300

    Fix Assert 2

--- Tests/Assert.ps1
+++ Tests/Assert.ps1
@@ -3,10 +3,12 @@
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Branch = 'Test' #'master'
 $Repository = 'Assert'
-$PathZip = (Join-Path $env:TEMP "$Repository-$Branch.zip")
-Invoke-WebRequest "https://github.com/ili101/$Repository/archive/$Branch.zip" -OutFile $PathZip
-#[Net.ServicePointManager]::SecurityProtocol = $SecurityProtocol
+$PathZip = Join-Path $env:TEMP "$Repository-$Branch.zip"
+$PathPsd1 = Join-Path $env:TEMP "$Repository-$Branch" | Join-Path -ChildPath "$Repository.psd1"
 
-Expand-Archive -Path $PathZip -DestinationPath $env:TEMP
-
-Import-Module -Name (Join-Path $env:TEMP "$Repository-$Branch" | Join-Path -ChildPath "$Repository.psd1")
+if (!(Test-Path $PathPsd1)) {
+    Invoke-WebRequest "https://github.com/ili101/$Repository/archive/$Branch.zip" -OutFile $PathZip
+    #[Net.ServicePointManager]::SecurityProtocol = $SecurityProtocol
+    Expand-Archive -Path $PathZip -DestinationPath $env:TEMP
+}
+Import-Module -Name $PathPsd1
